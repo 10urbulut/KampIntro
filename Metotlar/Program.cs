b80ca8d483ecad7ee8aca4f1d7d32589d2efdc26
10urb <10urbulut@gmail.com>
2021-04-23 16:43:45 +0300

    23 nisan ders sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meyveler = new string[] { "Elma", "Karpuz" };
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------");
+            }
+
+            Console.WriteLine("-------Metotlar-----------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle(urun1);
+
+            sepetManager.Ekle2("Armut","Yeşil",22,50);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır", 2992,96);
+            sepetManager.Ekle2("Elma", "Sarı", 252,26);
+        }
+    }
+}
